17712dbc27100bebc43a7e88e170555844a3aa97
simpleflow1 <hudsonextreme@gmail.com>
2026-03-18 16:19:09 -0400

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('SimpleFlow backend rodando 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
